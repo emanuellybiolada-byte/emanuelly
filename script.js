03ee977bae038ce4dad86bb2b187bdbc5b60dad2
emanuellybiolada-byte <emanuelly.biolada@escola.pr.gov.br>
2026-03-25 09:19:52 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
-
+document.querySelector('form').addEventListener('submit', function(event) {
+    event.preventDefault(); // Isso impede que o HTML envie o formulário do jeito padrão
+    
+    // Seu código de salvamento aqui...
+});
