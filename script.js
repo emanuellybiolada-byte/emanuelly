4b8c2fc4407cddebe8712a21c09a7c78c7d78314
emanuellybiolada-byte <emanuelly.biolada@escola.pr.gov.br>
2026-03-26 08:50:30 -0300

    .

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
-document.querySelector('form').addEventListener('submit', function(event) {
-    event.preventDefault(); // Isso impede que o HTML envie o formulário do jeito padrão
-    
-    // Seu código de salvamento aqui...
-});
+// Função para trocar de tela
+function nextStep(currentId, nextId) {
+    document.getElementById(currentId).classList.remove('active');
+    document.getElementById(nextId).classList.add('active');
+}
+
+// Lógica do Login
+document.getElementById('form-login').addEventListener('submit', function(e) {
+    e.preventDefault();
+    const user = document.getElementById('user').value;
+    const pass = document.getElementById('pass').value;
+
+    if(user === 'admin' && pass === '1234') {
+        nextStep('step-login', 'step-
